8ce6d0335582b8e3f7e7b2ad956d77976a57aa6d
DinhHuy <huy.dq@rockyhub.io>
2025-04-08 02:03:37 +0700

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: 'avatars.githubusercontent.com' },
+      { hostname: 'avatar.vercel.sh' },
+      { hostname: 'lh3.googleusercontent.com' },
+      { hostname: 'cdn.meetyapp.io' },
+    ],
+  },
+  env: {
+    API_URL: process.env.API_URL,
+    MEETY_ENV: process.env.NODE_ENV,
+    CDN_URL: process.env.CDN_URL,
+    PUBLIC_ASSET_VERSION: process.env.PUBLIC_ASSET_VERSION,
+  },
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,43 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screen: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1408px",
+    },
+    fontFamily: {
+      oswald: "var(--font-oswald)",
+      roboto: "var(--font-roboto)",
+    },
+    backgroundImage: {
+      hero: "url(/assets/img/hero/bg.png)",
+      membership: "url(/assets/img/membership/bg.png)",
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#333",
+          100: "#484848",
+          200: "#151515",
+          300: "#111",
+        },
+        accent: "#d4000d",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
