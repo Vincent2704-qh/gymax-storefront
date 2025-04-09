d9f007330dcbda1a0c12b8a712f9e3e90abf2b1e
DinhHuy <huy.dq@rockyhub.io>
2025-04-09 18:12:10 +0700

    add about & achivement component

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,7 +1,87 @@
+"use client";
+
+import { FaUser } from "react-icons/fa";
+import { IoIosPricetag } from "react-icons/io";
+import { FaDumbbell } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../lib/variants";
+import Achievements from "./Achievements";
+
+const featured = [
+  {
+    icon: <FaUser />,
+    title: "award-winning trainers",
+    subtile: "Something here, let me check again.",
+  },
+  {
+    icon: <IoIosPricetag />,
+    title: "excellent prices",
+    subtile: "Something here, let me check again.",
+  },
+  {
+    icon: <FaDumbbell />,
+    title: "modern equipment",
+    subtile: "Something here, let me check again.",
+  },
+];
+
 const About = () => {
   return (
-    <section className="h-screen bg-yellow-300" id="about">
-      About
+    <section className="pt-8 pb-14 lg:pt-16 lg:pb-28" id="about">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center gap-2 mb-8">
+          <motion.h2
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.2 }}
+            className="h2 text-center"
+          >
+            About us
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.2 }}
+            className="max-w-[600px] mx-auto text-center"
+          >
+            About something, this is a test i will check again
+          </motion.p>
+        </div>
+        <motion.div
+          variants={fadeIn("up", 0.8)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-16"
+        >
+          {featured.map((feature, index) => {
+            return (
+              <div
+                className="flex flex-col justify-center items-center gap-4 border p-10"
+                key={index}
+              >
+                <div className="text-4xl bg-primary-300 text-white w-[80px] h-[80px] rounded-full flex justify-center items-center">
+                  {feature.icon}
+                </div>
+                <div className="flex flex-col justify-center items-center gap-2 text-center">
+                  <h4 className="h4 text-accent">{feature.title}</h4>
+                  <p>{feature.subtile}</p>
+                </div>
+              </div>
+            );
+          })}
+        </motion.div>
+        <motion.div
+          variants={fadeIn("up", 1)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+        >
+          <Achievements />
+        </motion.div>
+      </div>
     </section>
   );
 };
